99e7b8f66515aa9aff7f9f8bfe477d02de9c158c
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2024-09-29 23:17:37 -0300

    iniciando o codigo dos coletaveis

---
+++ src/collectables/collectables.h
@@ -0,0 +1,11 @@
+#ifndef COLLECTABLES_H
+#define COLLECTABLES_H
+
+void collectables_create();
+
+void collectables_draw();
+
+// retorna a pontuação do objeto destruido
+int collectables_destroy();
+
+#endif
